30b7903c281b3bf8a22c247e355012b0866e540d
weic777 <anna0826777@gmail.com>
2025-08-20 16:37:59 +0800

    0820

--- react-poject/src/components/Todo.jsx
+++ react-poject/src/components/Todo.jsx
@@ -1,14 +1,26 @@
+import { MdDelete } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
 
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, delTodo ,toggleCompleted}) => {
     return (
-        <div className='todo'>
+        // 使用字串值+三元運算子的方法，控制是否套用css規則
+        <div className={`todo ${todo.isCompleted ? 'completed' : ''}`}>
             {/* <p>停車費</p> */}
-            <p>{todo}</p>
+            <p onClick={()=>{toggleCompleted(todo.id)}}>{todo.content}</p>
+            <div>
+                <MdEdit style={{ cursor: 'pointer', marginRight: '5px' }} />
+                <MdDelete
+                    style={{ cursor: 'pointer' }}
+                    onClick={() => { delTodo(todo.id) }}
+                />
+            </div>
         </div>
     )
 }
 
 
 export default Todo
+
+
 
--- react-poject/src/components/TodoWrapper.jsx
+++ react-poject/src/components/TodoWrapper.jsx
@@ -5,17 +5,65 @@ import Todo from './Todo'
 
 const TodoWrapper = () => {
     // 因為有N個todo，所以要用陣列存放
+    // const [todos, setTodos] = useState(['繳停車費', '對發票']);
+    // 由為陣列內容若有增減時，索引值會異動，
+    // 所以避免異動資料時造成索引值錯亂，將陣列改為陣列物件
+    // 而key值用亂數來產生
+
+
+    const [todos, setTodos] = useState(
+        [
+            { content: '繳停車費', id: Math.random(), isCompleted:false },
+            { content: '對發票', id: Math.random(), isCompleted:false },
+        ]
+    );
+
+
+    // 建立刪除todo
+    // 傳入被刪除的todo.id
+    const delTodo = (id) => {
+        setTodos(todos.filter((todo) => {
+            // 使用filter方法，保留不是被刪除的id
+            return todo.id !== id
+        }))
+    }
+
+
+    // 建立切換isCompleted屬性函式
     
-    const [todos, setTodos] = useState(['繳停車費', '對發票']);
+    const toggleCompleted=(id)=>{
+        // 檢查被點擊的項目的 id 是否跟陣列中的id一樣
+    // yes => 1. 取出todo 2. 將isCompleted屬性值反向(NOT)
+    // no  => todo 不變
+
+    setTodos(todos.map((todo)=>{
+        return todo.id===id
+        ?{...todo, isCompleted:!todo.isCompleted}
+        :todo
+    }))
+
+    }
 
 
     return (
         <div className='wrapper'>
             <h1>待辦事項</h1>
-            <CreateForm />
+            {/* 方法1：傳送todos陣列資料與setTodos方法到Todo元件 */}
+            <CreateForm todos={todos} setTodos={setTodos} />
+
+
+            {/* 方法2：將setTodos方法包裝成一個方法（帶有參數）到Todo元件 */}
+            {/*  
+            <CreateForm addTodo={(newContent) => {
+                // 建立新的todo內容
+                // 1. 使用...其餘運算子來保留原陣列內容
+                // 2. 再加上新的物件內容
+                setTodos([...todos, { content: newContent, id: Math.random(), isCompleted:false }])
+            }} />
+            */}
             {
-                todos.map((todo, index) => {
-                    return <Todo todo={todo} key={index} />
+                todos.map((todo) => {
+                    return <Todo todo={todo} key={todo.id} delTodo={delTodo} toggleCompleted={toggleCompleted}/>
                 })
             }
 
@@ -26,3 +74,5 @@ const TodoWrapper = () => {
 
 
 export default TodoWrapper
+
+
